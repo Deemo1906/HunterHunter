6c40e2ebbf2ab7df00e7d5c1c72aec48013b0456
Deemo1906 <74048075+Deemo1906@users.noreply.github.com>
2021-12-06 15:35:45 +0100

    sign

--- sign.php
+++ sign.php
@@ -1,12 +1,44 @@
-<<!DOCTYPE html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Hunter association official site</title>
 </head>
-<body>
-
+<body id="main">
+    <form action="login.php" method="post">
+    <div id = "login">
+        <img src="logo_main.png" alt="logos">
+        <br>
+        <tr>
+            <td id="text">Name:</td>
+            <br>
+            <td><input type="text" name="id"></td>
+        </tr>
+        <tr>
+            <td id="text">Pseudo:</td>
+            <br>
+            <td><input type="text" name="id"></td>
+        </tr>
+        <td id="text">Mail:</td>
+            <br>
+            <td><input type="text" name="id"></td>
+            <td id="text">Password</td>
+            <br>
+            <td><input type="password" name="passw"></td>
+        </tr>
+        <br>
+        <tr>
+        <br>
+        <td colspan="2" align="center">
+        <input type="submit" name="submit" value="Login">
+        </td>
+        </tr>
+        <h3 style="color: red;">Any unauthorized use of this site will result in deadly force</h3>
+    </div>
+    </form>
+    <script src="index.js"></script>
 </body>
 </html>
-
