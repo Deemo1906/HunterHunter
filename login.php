b07a59404c58e28e6ca57639298745a2c69712ac
FlorianPllt <florian.pelletier@edu.ece.fr>
2021-12-06 15:02:42 +0100

    Update login.php

--- login.php
+++ login.php
@@ -30,19 +30,19 @@
         if($count!=0) // nom d'utilisateur et mot de passe correctes
         {
            //$_SESSION['id'] = $id;
-           //header('Location: principale.php');
-        	echo"utilisateur connecté";
+           header('Location: index.html');
+        
         }
         else
         {
-           //header('Location: login.html?erreur=1'); // utilisateur ou mot de passe incorrect
-        	echo"identifiants incorrects";
+           header('Location: login.html?erreur=1'); // utilisateur ou mot de passe incorrect
+        	
         }
     }
     else
     {
-       //header('Location: login.html?erreur=2'); // utilisateur ou mot de passe vide
-    	echo"champs vides";
+       header('Location: login.html?erreur=2'); // utilisateur ou mot de passe vide
+    	
     }
 //fermer la connexion
 	mysqli_close($db_handle);
