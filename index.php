4433f7ae474ab44ea03ed9642f78789d36a1dffa
FlorianPllt <florian.pelletier@edu.ece.fr>
2021-12-08 11:20:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -46,6 +46,7 @@
             <a id="Notifications" onclick="change(this, event)">Notifications</a>
             <a id="My basket" onclick="change(this, event)">My basket</a>
             <a id="My account" onclick="change(this, event)">My account</a>
+            <a href='index.php?disconnect=true'id="Disconnect">Disconnect</a>
             <?php
                 session_start();
                 if(isset($_GET['disconnect']))
