88726728e64fd29585f16902e0f23c301f667a29
FlorianPllt <florian.pelletier@edu.ece.fr>
2021-12-08 16:33:04 +0100

    php ajout item

---
+++ Newitem.php
@@ -0,0 +1,78 @@
+<?php
+	session_start();
+	if(isset($_POST['name']) && isset($_POST['description'])&&isset($_POST['price'])&&isset($_POST['saletype'])&&isset($_POST['category'])&&isset($_POST['photo']))
+	{
+	echo"<meta charset=\"utf-8\">";
+	//echo "<link rel=\"stylesheet\"type=\"text/css\" href =\"dupondStyle.css\">";
+	//identifier base de donnée
+	$database = "hxh";
+
+	//se connecter à la BDD
+	//rappel: serveur = localhost/phpmyadmin login = root  mdp= Rien
+	$db_handle = mysqli_connect('localhost','root','');
+	$db_found = mysqli_select_db($db_handle, $database);
+
+	
+    $name = mysqli_real_escape_string($db_handle,htmlspecialchars($_POST['name'])); 
+    $description = mysqli_real_escape_string($db_handle,htmlspecialchars($_POST['desciption']));
+    $price =mysqli_real_escape_string($db_handle,htmlspecialchars($_POST['price']));;
+    $saletype = mysqli_real_escape_string($db_handle,htmlspecialchars($_POST['saletype'])); 
+    $category = mysqli_real_escape_string($db_handle,htmlspecialchars($_POST['category']));
+    $photo =mysqli_real_escape_string($db_handle,htmlspecialchars($_POST['photo']));;
+    
+    if($name !== "" && $description !== "" &&$price!=="" && $saletype !== "" && $category !== "" &&$photo!=="")
+    {
+        $requete = "SELECT count(*) FROM item where 
+              Name = '".$name."' ";
+        $exec_requete = mysqli_query($db_handle,$requete);
+        $reponse      = mysqli_fetch_array($exec_requete);
+        $count = $reponse['count(*)'];
+        if($count==0) // nom d'utilisateur et mot de passe correctes
+        {
+        	if($_SESSION['Atype']=="vendeur")
+        	{
+        		$sqlt = "SELECT IdVendeur FROM vendeur where Pseudo = '".$_SESSION['name']."'";
+        		$exec_sqlt = mysqli_query($db_handle,$sqlt);
+        		$data = mysqli_fetch_assoc($sqlt);
+            
+        		$sql = "INSERT INTO client (Name, Description, Price, SaleType,Category,Photo,IdAdmin,Idvendeur)
+	VALUES('$name','$Description','$price','$saletype','$category','photo','1','$data['IdVendeur']')";
+    		mysqli_query($db_handle,$sql);
+           header('Location: index.php');
+
+        }
+
+        if($_SESSION['Atype']=="admin")
+        	{
+        		$sqlt = "SELECT IdAdmin FROM admin where Pseudo = '".$_SESSION['name']."'";
+        		$exec_sqlt = mysqli_query($db_handle,$sqlt);
+        		$data = mysqli_fetch_assoc($sqlt);
+            
+        		$sql = "INSERT INTO client (Name, Description, Price, SaleType,Category,Photo,IdAdmin,Idvendeur)
+	VALUES('$name','$Description','$price','$saletype','$category','photo','$data['IdAdmin']','2')";
+    		mysqli_query($db_handle,$sql);
+           header('Location: index.php');
+
+        }
+        
+        }
+        else
+        {
+           //header('Location: Log.php?erreur=1'); // article déjà existant
+        	
+        }
+    }
+    else
+    {
+       //header('Location: Log.php?erreur=2'); // champ(s) vide(s)
+    	
+    }
+//fermer la connexion
+	mysqli_close($db_handle);
+
+	
+	}
+
+	?>
+
+	
