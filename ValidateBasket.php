f7b5afee9abdb5c288664242c9105e3a8d538bbf
FlorianPllt <florian.pelletier@edu.ece.fr>
2021-12-11 17:22:22 +0100

    mailvalide

--- ValidateBasket.php
+++ ValidateBasket.php
@@ -1,8 +1,14 @@
 <?php
 
-session_start();
+	session_start();
 	
 	echo"<meta charset=\"utf-8\">";
+
+	//paramètres mail
+	$header="MIME-Version: 1.0\r\n";
+	$header.='From:"HXH.com"<support@hxh.com>'."\n";
+	$header.='Content-Type:text/html; charset="uft-8"'."\n";
+	$header.='Content-Transfer-Encoding: 8bit';
 	
 	//identifier base de donnée
 	$database = "hxh";
@@ -21,7 +27,7 @@ session_start();
 	if($Atype=="client")
 	{
 
-	$sqlt = "SELECT IdClient,Adress,City,PostalCode,Country,Name FROM client where Pseudo = '".$_SESSION['name']."'";
+	$sqlt = "SELECT IdClient,Adress,City,PostalCode,Country,Name,Mail FROM client where Pseudo = '".$_SESSION['name']."'";
         		$exec_sqlt = mysqli_query($db_handle,$sqlt);
         		$data = mysqli_fetch_assoc($exec_sqlt);
 
@@ -31,6 +37,10 @@ session_start();
             $City=$data['City'];
             $PostalCode=$data['PostalCode'];
             $Country=$data['Country'];
+            $Mail=$data['Mail'];
+
+            $PriceT=0;
+            $NomItem="";
 
             echo"$idClient<br> $Name <br> $Adress<br>$City<br>$PostalCode<br>$Country<br>";
 
@@ -54,13 +64,19 @@ session_start();
 			$exec_sqlI = mysqli_query($db_handle,$sqlI);
 
 		}*/
-		$sqlI="SELECT item.Iditem,item.Price FROM item,panier,comporter WHERE panier.IdClient = '$idClient'and comporter.IdPanier=Panier.IdPanier and comporter.IdItem=item.Iditem";
+		$sqlI="SELECT item.Iditem,item.Price ,item.Name FROM item,panier,comporter WHERE panier.IdClient = '$idClient'and comporter.IdPanier=Panier.IdPanier and comporter.IdItem=item.Iditem";
 			$exec_sqlI = mysqli_query($db_handle,$sqlI);
 		//Inserer nouvelle(s) commande(s) si on l'on compte le même idpanier
 		while($dataI = mysqli_fetch_assoc($exec_sqlI))
 		{
 				$NumItem=$dataI['Iditem'];
 				$Price=$dataI['Price'];
+
+				$NameI=$dataI['Name'];
+
+				$PriceT=$PriceT + $Price;
+
+				$NomItem.=$NameI." ";
 
 				echo"$NumItem  $Price<br>";
 
@@ -75,53 +91,37 @@ session_start();
 
         echo"$count  $Idcommande<br><br>";
 
-        		//if($count==0)
-        		//{
+        		
             	
         		$sql = "INSERT INTO commande (Adress,City,PostalCode,Country,Price,NomAcheteur,NumItem,IdPanier)
 	VALUES('$Adress','$City','$PostalCode','$Country','$Price','$Name','$NumItem','$IdPanier')";
     		mysqli_query($db_handle,$sql);
-    		//}
-    		//else
-    		/*{
-    			$sql = "INSERT INTO commande (IdCommande,Adress,City,PostalCode,Country,Price,NomAcheteur,NumItem,IdPanier)
-	VALUES('$Idcommande','$Adress','$City','$PostalCode','$Country','$Price','$Name','$NumItem','$IdPanier')";
-    		mysqli_query($db_handle,$sql);
-
-    		}*/
-		}
-
-			///////////
-
-           /* $requete = "SELECT count(*) FROM panier where 
-              IdClient = '".$idClient."'  ";
-        $exec_requete = mysqli_query($db_handle,$requete);
-        $reponse      = mysqli_fetch_array($exec_requete);
-        $count = $reponse['count(*)'];
-
-        		if($count==0)
-        		{
-            	
-        		$sql = "INSERT INTO panier (IdClient)
-	VALUES('$idClient')";
-    		mysqli_query($db_handle,$sql);
-    		}
-
-    		///////Inserer dans comporter
-
-    		/*$sqlC = "SELECT Iditem FROM item WHERE Photo = '$imgName'";
-			$exec_sqlC = mysqli_query($db_handle,$sqlC);
-			$dataC = mysqli_fetch_assoc($exec_sqlC);
-			$Iditem = $dataC['Iditem'];
-
-			$sqlP = "SELECT IdPanier FROM panier WHERE IdClient = '$idClient'";
-			$exec_sqlP = mysqli_query($db_handle,$sqlP);
-			$dataP = mysqli_fetch_assoc($exec_sqlP);
-			$IdPanier = $dataP['IdPanier'];
-
-			$sqlComp="INSERT INTO comporter (IdItem,IdPanier) VALUES('$Iditem','$IdPanier') ";
-			mysqli_query($db_handle,$sqlComp);*/
+    		
     	}
+
+    	$message='<html>
+	<body>
+		<div align="center">
+			<br />
+			Congratulation for your purchase!!
+			<br /><br />
+			
+		</div>
+		<div align="left">
+			Summary: OrderN°:' .$IdPanier. ' , Price: ' .$PriceT.' , Items: '.$NomItem.'
+			<br><br> 
+
+		</div align="left">
+		<div>
+				Location: Adress: '.$Adress.' , City: '.$City. ' , PostalCode: '.$PostalCode.' , Country: '.$Country.'
+		</div>
+	 </body>
+   </html>';
+
+  	mail($Mail, "Commande HXH", $message, $header);
+
+  }
+
     	header('Location: index.php');
     	mysqli_close($db_handle);
 	?>
